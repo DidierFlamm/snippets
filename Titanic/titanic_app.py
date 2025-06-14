d26bd31472033c9dc8135080339e44b52a7e0439
Didier Flamm <137876296+DidierFlamm@users.noreply.github.com>
2025-06-14 14:59:50 +0200

    chore

--- Titanic/titanic_app.py
+++ Titanic/titanic_app.py
@@ -115,5 +115,5 @@ elif page == pages[2]:
 
     st.write("### Prédictions")
 
-    code = """🚧 Bientôt la possibilité d'évaluer la probabilité de survie\nen fonction de vos paramètres."""
+    code = """🚧 Bientôt la possibilité d'évaluer la probabilité de survie\nen fonction de vos paramètres 🚧"""
     st.code(code, language="python")
